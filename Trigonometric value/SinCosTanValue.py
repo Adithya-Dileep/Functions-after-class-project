2552f875e807b1308c1b3b78acf4db16a8843cbd
Adithya-Dileep <Shugyo23@gmail.com>
2025-11-29 12:50:47 +0400

    Add files via upload

---
+++ Trigonometric value/SinCosTanValue.py
@@ -0,0 +1,11 @@
+import math
+angle_degrees = 45
+print("Angle Degree : ", angle_degrees)
+angle_radians = math.radians(angle_degrees)
+print("Angle Radians", angle_radians )
+sine_value = math.sin(angle_radians)
+print("Sine value", sine_value)
+cosine_value = math.cos(angle_radians)
+print("cosine value", cosine_value)
+tangent_value = math.tan(angle_radians)
+print("tangent value", tangent_value)
